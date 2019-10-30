160ee167b92ce2917332d41c336fdb655f35f108
robpal94 <robin.palkovits@5minds.de>
2019-10-30 13:43:25 +0100

    :truck: Rename Module to GDrive Tools

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
 setuptools.setup(
-  name="google-docs-library",
+  name="gdrive-tools",
   version="0.0.1",
   author="Robin Palkovits",
   author_email="robin.palkovits@5minds.de",
-  description="Lets the User Create New Documents in Nested Directories",
+  description="A collection of usefull tools to interact with the google drive/google docs api",
   packages=setuptools.find_packages(),
   classifiers=[
       "Programming Language :: Python :: 3",
