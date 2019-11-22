f14f719a0bb191d8f91d14a2131903e2c0f3d8b7
robpal94 <robin.palkovits@5minds.de>
2019-11-22 17:00:41 +0100

    :bug: Fix not Installed Subdependencies

--- setup.py
+++ setup.py
@@ -12,5 +12,8 @@ setuptools.setup(
       "License :: OSI Approved :: MIT License",
       "Operating System :: OS Independent",
   ],
+  install_requires=['google-api-python-client',
+    'google-auth-httplib2',
+    'google-auth-oauthlib'],
   python_requires='>=3.6',
 )
