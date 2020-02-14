76a69682f4c77e69b5d025f08b7967ea7ff5778d
robpal94 <robin.palkovits@5minds.de>
2020-02-14 10:03:43 +0100

    :bug: Fix the Examples

--- examples/copy_document_between_shared_drives.py
+++ examples/copy_document_between_shared_drives.py
@@ -23,18 +23,18 @@ def main():
   # Create the google drive tools client with your local credentials.
   googleDriveTools = gt.GDriveTools(creds)
 
+  # At first, we want to specify the name of our shared drive, which
+  # contains the file that we want to move to another shared drive.
+  sourceSharedDriveName = 'GDriveTools_Test'
+
   # In the first step, we want to create a new document, stored on
-  # the 'simple/test' path.
-  destinationPath = f'simple/test'
+  # the 'simple/test' path inside the shared drive with the given name.
+  destinationPath = f'{sourceSharedDriveName}/simple/test'
   docname = 'sample'
   googleDriveTools.createFile(destinationPath, docname, GoogleFiletypes.DOCUMENT)
 
   # Let the google drive api some time to process our request.
   sleep(0.5)
-
-  # At first, we want to specify the name of our shared drive, which
-  # contains the file that we want to copy to another shared drive.
-  sourceSharedDriveName = 'GDriveTools_Test'
 
   # Now we need to specify the sourcePath of the document, which we
   # want to copy. The path is described with with the usual syntax.
